8be7ae1a60f659d463e9231e8098ffb4b4da0e1b
Nicolas Winkler <nicolas.winkler@gmx.ch>
2018-09-18 11:54:01 +0200

    working on minimal prototype

--- project/extractor/src/main.rs
+++ project/extractor/src/main.rs
@@ -2,6 +2,9 @@
 #![feature(rustc_private)]
 //#![feature(rust_2018_preview)]
 
+//extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 use rustc_driver::{self, Compilation};
 use rustc_driver::driver::{CompileController, PhaseController, CompileState};
@@ -15,6 +18,8 @@ use rustc::hir::itemlikevisit::ItemLikeVisitor;
 use syntax::ast::Name;
 use syntax::source_map::Span;
 use std::env;
+
+pub mod data;
 
 
 fn main() {
@@ -53,6 +58,7 @@ exit(rustc_driver::run(move || {
         // we still want to be able to invoke it normally though
         orig_args.remove(1);
     }
+
     // this conditional check for the --sysroot flag is there so users can call
     // `clippy_driver` directly
     // without having to pass --sysroot or anything
@@ -67,6 +73,9 @@ exit(rustc_driver::run(move || {
             .collect()
     };
     
+    if !args.iter().any(|s| s.starts_with("--print=")) {
+        // may print stuff
+    }
 
 
     let mut controller = CompileController::basic();
@@ -76,15 +85,21 @@ exit(rustc_driver::run(move || {
         let crate_name_env = env::var("CARGO_PKG_NAME").unwrap_or_default();
         let crate_version_env = env::var("CARGO_PKG_VERSION").unwrap_or_default();
         let crate_name = cs.crate_name.unwrap();
+        if crate_name_env != crate_name {
+            println!("\x1b[31mdifferent names!: {}, {}\x1b[0m", crate_name_env, crate_name);
+        }
         let tcx = &cs.tcx.expect("no valid tcx");
         let hir_map = &tcx.hir;
         let ref krate = hir_map.krate();
         let mut cv = CrateVisitor {
+            crate_data: data::Crate::new(crate_name, (0, 0, 0)),
             crate_name: crate_name,
             map: hir_map
         };
 
         walk_crate(&mut cv, krate);
+
+        println!("{:?}", cv.crate_data);
 
         /*let mut clv = CrateLikeVisitor {
             map: hir_map
@@ -100,6 +115,7 @@ exit(rustc_driver::run(move || {
 
 struct CrateVisitor<'tcx, 'a>
 {
+    crate_data: data::Crate,
     crate_name: &'a str,
     map: &'tcx Map<'tcx>
 }
@@ -108,13 +124,42 @@ impl<'tcx, 'a> Visitor<'tcx> for CrateVisitor<'tcx, 'a> {
     fn visit_name(&mut self, _: Span, name: Name) {
     }
 
+    fn visit_mod(&mut self, m: &'tcx hir::Mod, _s: Span, id: NodeId) {
+        let maybe_node = self.map.find(id);
+        if let Some(hir::Node::Item(item)) = maybe_node {
+            let name: &str = &item.name.as_str();
+            let path = self.map.def_path(self.map.local_def_id(id));
+            self.crate_data.mods.push(data::Mod {
+                name: String::from(name),
+                functions: vec![],
+                parent_mod_id: Some(0) // TODO get this index
+            });
+            println!("{:?}", data::UniqueIdentifier::from_def_path_of_mod(&path));
+        }
+        else if let Some(hir::Node::ForeignItem(item)) = maybe_node {
+            //let name: &str = &item.name.as_str();
+            //println!("visited foreign mod: {:?}", maybe_node);
+        }
+        else {
+            //println!("visited mod that is not an item: {:?}", maybe_node);
+        }
+        
+        walk_mod(self, m, id);
+    }
+
     fn visit_item(&mut self, item: &'tcx hir::Item) {
+        /*match &item.node {
+            hir::ItemKind::Mod(_m) => {
+                println!("module: {}", item.name);
+            },
+            _ => {}
+        }*/
         walk_item(self, item);
     }
 
     fn visit_fn(&mut self, fk: FnKind<'tcx>, fd: &'tcx hir::FnDecl, b: hir::BodyId, s: Span, id: NodeId) {
         //println!("visited function in crate {} {} {:?} {:?}", self.crate_name, self.map.def_path_from_id(id).expect("invalid node traversed").to_string_no_crate(), fd, id);
-        println!("visited function in crate {} {:?} {:?} {:?}", self.crate_name, self.map.def_path_from_id(id).expect("invalid node traversed").data, fd, id);
+        //println!("visited function in crate {} {:?} {:?} {:?}", self.crate_name, self.map.def_path_from_id(id).expect("invalid node traversed").data, fd, id);
         walk_fn(self, fk, fd, b, s, id);
     }
 
