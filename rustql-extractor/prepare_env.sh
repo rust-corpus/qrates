1adcd35eb174e72a0854c9e4f7787f630b826725
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-01-10 21:10:55 +0100

    Add the licensing comment to all files.

--- rustql-extractor/prepare_env.sh
+++ rustql-extractor/prepare_env.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
 CRATE_PATH="`dirname \"$0\"`"
 
 # variables for compilations
@@ -8,6 +14,3 @@ export RUSTC_BACKTRACE=1
 
 # target directory for extracted data
 export EXTRACTOR_TARGET_DIR=$HOME/.rustql/crates
-
-
-
--- rustql-query/template.rs
+++ rustql-query/template.rs
@@ -1,3 +1,9 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // rustc template.rs -L crate=../rustql-common/target/debug/build/ -L ../rustql-common/target/debug/deps/ --crate-type=lib -O
 extern crate rustql_common;
 
