a4d77c64fee220fb35ea06f4bbec53f142ce791d
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-01-14 21:19:02 +0100

    rustfmt and fix compiler warnings

--- rustql-linker/src/main.rs
+++ rustql-linker/src/main.rs
@@ -2,19 +2,14 @@
 // http://opensource.org/licenses/MIT>. This file may not be copied,
 // modified, or distributed except according to those terms.
 
-extern crate rustql_common;
-extern crate datafrog;
-extern crate serde_json;
 extern crate bincode;
+extern crate datafrog;
+extern crate rustql_common;
+extern crate serde_json;
 
-
-
-use rustql_common::tuples;
 use rustql_common::data;
-use std::collections::HashMap;
-use datafrog::{Iteration, Relation, Variable};
+use rustql_common::tuples;
 use std::fs::File;
-
 
 const USE_JSON: bool = false;
 
@@ -32,31 +27,59 @@ fn save_database(database: &tuples::Database, name: &str) {
     bincode::serialize_into(file, database).expect("could not serialize to json");
 }
 
-
 fn create_database() -> tuples::Database {
     let mut database = tuples::Database::new();
     let crates = read_crates();
-    database.crates = crates.iter().map(|c| c.metadata.clone()).zip(0..).map(|(a, b)| (tuples::Crate(b), a)).collect();
+    database.crates = crates
+        .iter()
+        .map(|c| c.metadata.clone())
+        .zip(0..)
+        .map(|(a, b)| (tuples::Crate(b), a))
+        .collect();
 
     for (krate, krate_id) in crates.iter().zip(0..) {
         let mod_offset = database.modules.len();
         let fn_offset = database.functions.len();
-        let ty_offset = database.types.len();
 
         let module_map_to_global = |index: usize| index + mod_offset;
         let fn_map_to_global = |index: usize| index + fn_offset;
 
-        database.modules.extend(krate.mods.iter().zip(
-                mod_offset..).map(|(a, b)| (tuples::Mod(b as u64), a.clone())));
-        database.functions.extend(krate.functions.iter().zip(
-                database.functions.len()..).map(|(a, b)| (tuples::Function(b as u64), a.clone())));
-        database.function_finder.extend(krate.functions.iter().zip(
-                database.function_finder.len()..).map(|(a, b)| ((krate.metadata.clone(), a.def_path.clone()), (tuples::Function(b as u64)))));
+        database.modules.extend(
+            krate
+                .mods
+                .iter()
+                .zip(mod_offset..)
+                .map(|(a, b)| (tuples::Mod(b as u64), a.clone())),
+        );
+        database.functions.extend(
+            krate
+                .functions
+                .iter()
+                .zip(database.functions.len()..)
+                .map(|(a, b)| (tuples::Function(b as u64), a.clone())),
+        );
+        database.function_finder.extend(
+            krate
+                .functions
+                .iter()
+                .zip(database.function_finder.len()..)
+                .map(|(a, b)| {
+                    (
+                        (krate.metadata.clone(), a.def_path.clone()),
+                        (tuples::Function(b as u64)),
+                    )
+                }),
+        );
 
-        database.structs.extend(krate.structs.iter().zip(
-                database.structs.len()..).map(|(a, b)| (tuples::Struct(b as u64), a.clone())));
+        database.structs.extend(
+            krate
+                .structs
+                .iter()
+                .zip(database.structs.len()..)
+                .map(|(a, b)| (tuples::Struct(b as u64), a.clone())),
+        );
         /*database.types.extend(krate.types.iter().zip(
-                ty_offset..).map(|(a, b)| (tuples::Type(b as u64), a.clone())));*/
+        ty_offset..).map(|(a, b)| (tuples::Type(b as u64), a.clone())));*/
 
         for (m, mod_id) in krate.mods.iter().zip((0..).map(module_map_to_global)) {
             let tuple = (tuples::Mod(mod_id as u64), tuples::Crate(krate_id));
@@ -69,11 +92,18 @@ fn create_database() -> tuples::Database {
 
         for (f, fn_id) in krate.functions.iter().zip((0..).map(fn_map_to_global)) {
             let parent_id = module_map_to_global(f.containing_mod);
-            let tuple = (tuples::Function(fn_id as u64), tuples::Mod(parent_id as u64));
+            let tuple = (
+                tuples::Function(fn_id as u64),
+                tuples::Mod(parent_id as u64),
+            );
             database.functions_in_modules.push(tuple);
         }
     }
-    println!("created all entries for static tables: {} functions, {} structs", database.functions.len(), database.structs.len());
+    println!(
+        "created all entries for static tables: {} functions, {} structs",
+        database.functions.len(),
+        database.structs.len()
+    );
     println!("starting linking function calls");
 
     // create function call links
@@ -82,11 +112,12 @@ fn create_database() -> tuples::Database {
         for call in &func.calls {
             //let crate_id = database.get_crate(&call.crate_ident).unwrap();
             //let called_id = database.get_function_in_crate(crate_id, &call.def_path);
-            let called_id = database.function_finder.get(&(call.crate_ident.clone(), call.def_path.clone()));
+            let called_id = database
+                .function_finder
+                .get(&(call.crate_ident.clone(), call.def_path.clone()));
             if let Some(called_id) = called_id {
                 database.function_calls.push((*f_id, *called_id));
-            }
-            else {
+            } else {
                 // TODO find out why it didn't work
                 //println!("unresolved function call to {:?}", call.def_path);
                 //println!("fns: {:?}", database.functions);
@@ -94,7 +125,6 @@ fn create_database() -> tuples::Database {
             }
         }
 
-        
         for arg in &func.argument_types {
             let mut type_id = database.get_type(arg);
             if let None = type_id {
@@ -110,13 +140,21 @@ fn create_database() -> tuples::Database {
             println!("processed function no {}", f_id.0);
         }
     }
-    println!("linked all calls in #functions: {}", database.functions.len());
+    println!(
+        "linked all calls in #functions: {}",
+        database.functions.len()
+    );
     println!("#calls: {}", database.function_calls.len());
-    println!("ratio fails / calls: {}", fails as f64 / (database.function_calls.len() + fails) as f64);
+    println!(
+        "ratio fails / calls: {}",
+        fails as f64 / (database.function_calls.len() + fails) as f64
+    );
 
-    
     database.link_types();
-    println!("linked structs and types, created {} links.", database.is_struct_type.len());
+    println!(
+        "linked structs and types, created {} links.",
+        database.is_struct_type.len()
+    );
 
     // adding field types
     for (s_id, st) in &database.structs {
@@ -156,8 +194,8 @@ fn read_crates() -> Vec<data::Crate> {
     use std::fs;
     use std::fs::File;
 
-    let dirname = env::var(TARGET_DIR_VARNAME).unwrap_or(env::var("HOME").unwrap_or("/".to_owned()) +
-                                                         "/.rustql/crates");
+    let dirname = env::var(TARGET_DIR_VARNAME)
+        .unwrap_or(env::var("HOME").unwrap_or("/".to_owned()) + "/.rustql/crates");
 
     let files = fs::read_dir(dirname).unwrap();
     let mut crates: Vec<data::Crate> = vec![];
@@ -167,20 +205,16 @@ fn read_crates() -> Vec<data::Crate> {
             let f = File::open(path.path()).unwrap();
             let c = if USE_JSON {
                 serde_json::from_reader(f).map_err(|_| ())
-            }
-            else {
+            } else {
                 bincode::deserialize_from(f).map_err(|_| ())
             };
 
             if let Ok(cr) = c {
                 crates.push(cr);
-            }
-            else {
+            } else {
                 println!("ERROR deserializing crate {:?}: {:?}", path.path(), c);
             }
         }
     }
     crates
 }
-
-
