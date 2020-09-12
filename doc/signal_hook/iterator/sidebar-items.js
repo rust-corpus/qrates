92b463e851bae34a68397db29e2debd961aa7055
vakaras <vakaras@users.noreply.github.com>
2020-09-12 21:18:53 +0000

    deploy: 1e594bedae2772b743739388efc4526bbf2e4b1f

--- doc/signal_hook/iterator/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Forever","The infinite iterator of signals."],["Pending","The iterator of one batch of signals."],["Signals","The main structure of the module, representing interest in some signals."]]});
