56b25acb5cc32170816513edf65f030da67c4cdb
vakaras <vakaras@users.noreply.github.com>
2021-01-04 14:14:37 +0000

    deploy: 7b2740be9e5c5960000fba59783ebd556b97548a

--- doc/implementors/core/fmt/trait.Write.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["arrayvec"] = [{"text":"impl&lt;A:&nbsp;Array&lt;Item = u8&gt;&gt; Write for ArrayString&lt;A&gt;","synthetic":false,"types":[]}];
-implementors["bytes"] = [{"text":"impl Write for BytesMut","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
