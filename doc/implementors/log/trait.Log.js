38802cae529f04bc64a88bd029bdb883cd1cd34c
vakaras <vakaras@users.noreply.github.com>
2021-01-08 13:22:27 +0000

    deploy: e99163f4015055de4f6cab909559cfd1c664e2db

--- doc/implementors/log/trait.Log.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["env_logger"] = [{"text":"impl Log for Logger","synthetic":false,"types":[]}];
-implementors["simplelog"] = [{"text":"impl Log for CombinedLogger","synthetic":false,"types":[]},{"text":"impl Log for SimpleLogger","synthetic":false,"types":[]},{"text":"impl Log for TermLogger","synthetic":false,"types":[]},{"text":"impl&lt;W:&nbsp;Write + Send + 'static&gt; Log for WriteLogger&lt;W&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
