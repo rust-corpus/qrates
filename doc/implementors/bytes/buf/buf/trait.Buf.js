614e6b18bd264d265e3802e89cc9f37e01f8cc91
vakaras <vakaras@users.noreply.github.com>
2021-01-08 13:11:21 +0000

    deploy: d06af51744f6eed43c7acfd42a7280140afedebd

---
+++ doc/implementors/bytes/buf/buf/trait.Buf.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["hyper"] = [{"text":"impl Buf for Chunk","synthetic":false,"types":[]}];
+implementors["reqwest"] = [{"text":"impl Buf for Chunk","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
