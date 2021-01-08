7b1b7288770bdf25758b8c9bd5a4d258984e28ea
vakaras <vakaras@users.noreply.github.com>
2021-01-08 13:04:39 +0000

    deploy: d06af51744f6eed43c7acfd42a7280140afedebd

--- doc/corpus_manager_driver/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Command",""]],"fn":[["main",""]],"struct":[["CorpusManagerArgs",""]]});
--- doc/semver_parser/semver_parser/version/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Identifier",""]],"fn":[["parse",""]],"struct":[["Version",""]]});
