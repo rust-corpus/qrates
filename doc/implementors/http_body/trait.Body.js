05ffebf0126196279fd323934c6bf655fc126274
vakaras <vakaras@users.noreply.github.com>
2021-01-08 12:56:53 +0000

    deploy: 1dd877d40cf440998139f2941104c033c7d5baf7

---
+++ doc/implementors/http_body/trait.Body.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["http_body"] = [];
+implementors["hyper"] = [{"text":"impl Body for Body","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/implementors/serde/ser/trait.Serializer.js
@@ -0,0 +1,6 @@
+(function() {var implementors = {};
+implementors["bincode"] = [{"text":"impl&lt;'a, W:&nbsp;Write, O:&nbsp;Options&gt; Serializer for &amp;'a mut Serializer&lt;W, O&gt;","synthetic":false,"types":[]}];
+implementors["serde_json"] = [{"text":"impl&lt;'a, W, F&gt; Serializer for &amp;'a mut Serializer&lt;W, F&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;W: Write,<br>&nbsp;&nbsp;&nbsp;&nbsp;F: Formatter,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl Serializer for Serializer","synthetic":false,"types":[]}];
+implementors["serde_urlencoded"] = [{"text":"impl&lt;'input, 'output, Target&gt; Serializer for Serializer&lt;'input, 'output, Target&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Target: 'output + UrlEncodedTarget,&nbsp;</span>","synthetic":false,"types":[]}];
+implementors["toml"] = [{"text":"impl&lt;'a, 'b&gt; Serializer for &amp;'b mut Serializer&lt;'a&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
