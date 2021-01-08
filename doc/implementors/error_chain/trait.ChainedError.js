9fa2c85e1ffca9e580a95413ecd7acf78f347554
vakaras <vakaras@users.noreply.github.com>
2021-01-08 17:35:09 +0000

    deploy: 13a2fec79d423c4bd5bd0154b8627f81aee936d0

--- doc/implementors/error_chain/trait.ChainedError.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["crates_index"] = [{"text":"impl ChainedError for Error","synthetic":false,"types":[]}];
-implementors["error_chain"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/pin_project/trait.UnsafeUnpin.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["tracing_futures"] = [{"text":"impl&lt;T&gt; UnsafeUnpin for Instrumented&lt;T&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
