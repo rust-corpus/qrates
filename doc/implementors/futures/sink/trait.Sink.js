a9948b4ef8c42596b1c143f877a97efb66a21c61
vakaras <vakaras@users.noreply.github.com>
2021-01-14 07:58:57 +0000

    deploy: dfeb9e0c653fed0662a18679f1905dab776a5670

---
+++ doc/implementors/futures/sink/trait.Sink.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["futures"] = [];
+implementors["tokio_sync"] = [{"text":"impl&lt;T&gt; Sink for Sender&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; Sink for UnboundedSender&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; Sink for Sender&lt;T&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
