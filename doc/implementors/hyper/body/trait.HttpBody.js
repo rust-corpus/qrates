9cc57b445c11bfe8fb2c4ec7c04cd1fa5f7bba11
vakaras <vakaras@users.noreply.github.com>
2021-01-14 07:53:44 +0000

    deploy: dfeb9e0c653fed0662a18679f1905dab776a5670

--- doc/implementors/hyper/body/trait.HttpBody.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["hyper"] = [{"text":"impl Body for Body","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
