67425d6dac6b8e0673c7caaf7343b3bc8fcba718
vakaras <vakaras@users.noreply.github.com>
2021-01-15 09:07:04 +0000

    deploy: f770138d0da0d7991057e7f1ee227aba60efb099

---
+++ doc/implementors/tokio_executor/park/trait.Unpark.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["tokio_executor"] = [];
+implementors["tokio_reactor"] = [{"text":"impl Unpark for Handle","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
