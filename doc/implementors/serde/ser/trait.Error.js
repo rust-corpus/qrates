11e7106bc640a07bfadb48338055d45e1275d21d
vakaras <vakaras@users.noreply.github.com>
2021-01-19 08:51:20 +0000

    deploy: e6607e73e1515cd8b5d60494a9094cbb77b8561e

--- doc/implementors/serde/ser/trait.Error.js
+++
@@ -1,8 +0,0 @@
-(function() {var implementors = {};
-implementors["bincode"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["csv"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde"] = [];
-implementors["serde_json"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde_urlencoded"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["toml"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
