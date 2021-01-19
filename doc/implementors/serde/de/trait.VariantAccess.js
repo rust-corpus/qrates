a7eccc49a80377f74b8e87564fb2e9017d88e969
vakaras <vakaras@users.noreply.github.com>
2021-01-19 08:56:30 +0000

    deploy: e6607e73e1515cd8b5d60494a9094cbb77b8561e

---
+++ doc/implementors/serde/de/trait.VariantAccess.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["bincode"] = [{"text":"impl&lt;'de, 'a, R, O&gt; VariantAccess&lt;'de&gt; for &amp;'a mut Deserializer&lt;R, O&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;R: BincodeRead&lt;'de&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;O: Options,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
