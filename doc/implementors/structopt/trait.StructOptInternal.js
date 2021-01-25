6576931713ffc5fff785d54fc883dfd7bfdef0bc
vakaras <vakaras@users.noreply.github.com>
2021-01-25 12:31:39 +0000

    deploy: db3dfee2f3e9361699dcf1ad4276aa74ac6ed214

--- doc/implementors/structopt/trait.StructOptInternal.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["corpus_manager_driver"] = [{"text":"impl StructOptInternal for CorpusManagerArgs","synthetic":false,"types":[]},{"text":"impl StructOptInternal for Command","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
