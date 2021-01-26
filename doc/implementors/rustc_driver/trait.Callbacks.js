e0e0e549a826c00939ecd6a55c21fb52b80bd43e
vakaras <vakaras@users.noreply.github.com>
2021-01-26 11:15:19 +0000

    deploy: 721a7f5aed1101978d0ac4919e0bad3dd27e1418

--- doc/implementors/rustc_driver/trait.Callbacks.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rustc"] = [{"text":"impl Callbacks for CorpusCallbacks","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
