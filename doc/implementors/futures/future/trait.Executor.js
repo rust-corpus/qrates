699f94317458d0b071dc373c33450c4b1e21f34d
vakaras <vakaras@users.noreply.github.com>
2021-02-12 08:52:18 +0000

    deploy: 4a2ad88fdd7e3aa1a3bc6b6131a8d68eef310eea

---
+++ doc/implementors/futures/future/trait.Executor.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["tokio_executor"] = [{"text":"impl&lt;T&gt; Executor&lt;T&gt; for DefaultExecutor <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Future&lt;Item = (), Error = ()&gt; + Send + 'static,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
