55d86411547d3e54bc15379f2a780df6c8a692c5
vakaras <vakaras@users.noreply.github.com>
2021-03-05 11:11:29 +0000

    deploy: 0711c10f83a54ec8d09aeed6c025aa7099ba42aa

---
+++ doc/implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["crates_index"] = [{"text":"impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"crates_index/struct.Error.html\" title=\"struct crates_index::Error\">Error</a>","synthetic":false,"types":["crates_index::Error"]}];
+implementors["error_chain"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
