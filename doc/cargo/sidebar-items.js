29e59eb9b935f3e56185eed8c1e856798b1d732f
vakaras <vakaras@users.noreply.github.com>
2021-04-02 11:05:24 +0000

    deploy: 69f405af85d081c72c70a4885761a6e7ba262f6f

---
+++ doc/cargo/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["CARGO_ENV",""]],"fn":[["exit_with_error",""],["handle_error",""],["print_json",""],["version",""]],"mod":[["core",""],["ops",""],["sources",""],["util",""]],"struct":[["CfgInfo",""],["CommitInfo",""],["VersionInfo",""]]});
