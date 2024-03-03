ccbbad321b0a6862490f4a6a71808f6e020ba0b4
Vytautas Astrauskas <vastrauskas@gmail.com>
2024-03-03 12:55:30 +0100

    Create ungzip script.

---
+++ grep/README.md
@@ -0,0 +1,12 @@
+# Grepping crates.io
+
+## Download all crates
+
+```bash
+git clone https://github.com/the-lean-crate/criner
+cd criner
+cargo run —release — mine
+```
+
+The crates will be stored in `criner.db` directory.
+
---
+++ grep/ungzip.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+def main(input_root, output_root):
+    for root, dirs, files in os.walk(input_root):
+        for file in files:
+            if file.endswith('.crate'):
+                path = os.path.join(root, file)
+                output_path = os.path.join(output_root, file)
+                print(f'Extracting {path} to {output_path}')
+                # Create output directory.
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
+                # Extract all *.rs files from the crate.
+                os.system(f"tar -xzf {path} --wildcards '*.rs' -C {output_path}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: ungzip.py <input_root> <output_root>')
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
