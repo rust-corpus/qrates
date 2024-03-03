87e4ae6d80ff2373e9e5e6609731227e6e1d5fa2
Vytautas Astrauskas <vastrauskas@gmail.com>
2024-03-03 15:56:06 +0100

    Improve ungzip script.

--- grep/README.md
+++ grep/README.md
@@ -10,3 +10,18 @@ cargo run —release — mine
 
 The crates will be stored in `criner.db` directory.
 
+## Extract all crates
+
+Directory `criner` is where Criner repository was cloned. Directory `qrates` is where Qrates repository was cloned.
+
+```bash
+python3 qrates/grep/ungzip.py criner/criner.db/assets/ extracted-crates >> log
+```
+
+## Grep
+
+Find all files that mention `unsafe `:
+
+```bash
+rg --files-with-matches 'unsafe ' extracted-crates/ > unsafe-files.csv
+```
--- grep/ungzip.py
+++ grep/ungzip.py
@@ -1,18 +1,25 @@
 import os
 import sys
+import datetime
 
 def main(input_root, output_root):
     for root, dirs, files in os.walk(input_root):
         for file in files:
             if file.endswith('.crate'):
                 path = os.path.join(root, file)
-                output_path = os.path.join(output_root, file)
-                command = f"tar -xzf {path} --wildcards '*.rs' -C {output_path}"
-                # Create output directory.
-                os.makedirs(os.path.dirname(output_path), exist_ok=True)
-                # Extract all *.rs files from the crate.
-                os.system(command)
-
+                oroot = root[len(input_root):]
+                output_path = os.path.join(output_root, oroot, file)
+                if os.path.exists(output_path):
+                    print(f"Already exists: {output_path}")
+                else:
+                    command = f"tar -xzf {path} -C {output_path} --wildcards --no-anchored '*.rs' --wildcards Cargo.toml"
+                    # Create output directory.
+                    os.makedirs(output_path, exist_ok=True)
+                    # Extract all *.rs files from the crate.
+                    print(datetime.datetime.now(), command)
+                    exit_code = os.system(command)
+                    if exit_code != 0:
+                        print(f"Failed with error: {exit_code}")
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
